b2feb14bbcbefa698a8f999ab53b7392e691f5d6
phamlucchuong <lucchuongg@gmail.com>
2025-09-13 18:21:26 +0700

    first commit

---
+++ src/main/java/com/example/BIGFOOD/BigfoodBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.BIGFOOD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigfoodBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigfoodBeApplication.class, args);
+	}
+
+}
